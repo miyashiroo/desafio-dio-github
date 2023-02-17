0afcfd70a3583a71a131e120a0fffa8ab2c51bcd
miyashiroo <miyashiroo@gmail.com>
2023-02-17 13:23:15 -0400

    Classe livro

--- 01 - Dominando-IDEs-Java/primeiro-programa-intellij/src/br/com/dio/PrimeiroProgramaIntellij.java
+++ 01 - Dominando-IDEs-Java/primeiro-programa-intellij/src/br/com/dio/PrimeiroProgramaIntellij.java
@@ -10,8 +10,42 @@ public class PrimeiroProgramaIntellij {
         System.out.println("Hello World Galere!" + (a+b));
     }*/
         Gato gato = new Gato();
-
+        Livro livro = new Livro("Algoritmos iniciante", 300);
+        System.out.println(livro);
         System.out.println(gato);
     }
 }
 
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+}
