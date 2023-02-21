ddb77a0cdb39252b1695eaa3d450619e77633c79
miyashiroo <miyashiroo@gmail.com>
2023-02-21 16:40:52 -0400

    Boas Praticas

---
+++ 03 - Condicionais Logicas e Controle de Fluxo/BoasPraticas/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        /*
+        Switch é para valores exatos
+        IF para expressões booleanas
+
+        Evitar usar o default do switch para "cases genericos";
+            Usar para casos que podem dar erro.
+
+        Evitar oe feito "Flecha" dos if's
+            >
+                >
+                    >
+                        >
+                    >
+                >
+            >
+
+        Evitar muitos if's aninhados
+         */
+    }
+}
---
+++ 03 - Condicionais Logicas e Controle de Fluxo/ControleDeFluxo/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+        Decisão: if,if-else, if-else-if, switch, operador ternário
+        Repetição : for, while,  do while;
+        Interrupção: break, continue e return;
+
+      **  Evitar ao maximo o uso de operadores ternários; **
+
+
+         */
+
+    }
+}
