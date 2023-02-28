d8ff9c0dd83237a2491d6ee64000ce93f1c8dc52
miyashiroo <miyashiroo@gmail.com>
2023-02-28 10:40:41 -0400

    estrutura de dados

---
+++ 05 - Estrutura de Dados em Java/untitled/src/EstruturaDeDados.java
@@ -0,0 +1,10 @@
+public class EstruturaDeDados {
+    public static void main(String[] args) {
+
+        /*
+        bit -> menor quantiddade de memoria.
+        memoria primária -> memoria ram, só guarda memoria na presença de energia
+        memoria secundaria -> hd , guarda memoria sem energia.
+         */
+    }
+}
