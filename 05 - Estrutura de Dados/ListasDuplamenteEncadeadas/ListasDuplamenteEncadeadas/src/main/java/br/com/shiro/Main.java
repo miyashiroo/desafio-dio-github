107b2e2c32cdb86191bd082cc47618da22180c53
Rodrigo Miyashiro <48845871+miyashiroo@users.noreply.github.com>
2023-03-09 15:37:41 -0400

    listas circulares

---
+++ 05 - Estrutura de Dados/ListasDuplamenteEncadeadas/ListasDuplamenteEncadeadas/src/main/java/br/com/shiro/Main.java
@@ -0,0 +1,26 @@
+package br.com.shiro;
+
+public class Main {
+    public static void main(String[] args) {
+        ListaDuplamenteEncadeada<String> minhaListaEncadeada = new ListaDuplamenteEncadeada<>();
+
+        minhaListaEncadeada.add("c1");
+        minhaListaEncadeada.add("c2");
+        minhaListaEncadeada.add("c3");
+        minhaListaEncadeada.add("c4");
+        minhaListaEncadeada.add("c5");
+        minhaListaEncadeada.add("c6");
+        minhaListaEncadeada.add("c7");
+
+        System.out.println(minhaListaEncadeada);
+
+
+        minhaListaEncadeada.add(3,"RiSOS");
+
+        System.out.println(minhaListaEncadeada);
+
+        System.out.println(minhaListaEncadeada.get(2));
+
+
+    }
+}
