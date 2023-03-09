0016bf575f54d9a1287541312fba1d48ccda36a2
Rodrigo Miyashiro <48845871+miyashiroo@users.noreply.github.com>
2023-03-09 16:20:36 -0400

    listas circulares

--- 05 - Estrutura de Dados/ListasCirculares/ListasCirculares/src/main/java/org/example/Main.java
+++ 05 - Estrutura de Dados/ListasCirculares/ListasCirculares/src/main/java/org/example/Main.java
@@ -2,6 +2,26 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ListaCircular<String> minhaListaCircular = new ListaCircular<>();
+
+        minhaListaCircular.add("c0");
+        System.out.println(minhaListaCircular);
+
+        minhaListaCircular.remove(0);
+        System.out.println(minhaListaCircular);
+        minhaListaCircular.add("c1");
+        minhaListaCircular.add("c2");
+
+        System.out.println(minhaListaCircular);
+        minhaListaCircular.add("c3");
+
+        System.out.println(minhaListaCircular);
+
+        System.out.println(minhaListaCircular.get(0));
+        System.out.println(minhaListaCircular.get(1));
+        System.out.println(minhaListaCircular.get(2));
+        System.out.println(minhaListaCircular.get(6));
+
     }
 }
