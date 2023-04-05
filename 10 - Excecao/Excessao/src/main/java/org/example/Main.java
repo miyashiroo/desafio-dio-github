9b95d47a467b193940b7aaee1c7d479ff4bebb79
Rodrigo Miyashiro <48845871+miyashiroo@users.noreply.github.com>
2023-04-05 15:25:11 -0400

    excecao

--- 10 - Excecao/Excessao/src/main/java/org/example/Main.java
+++ 10 - Excecao/Excessao/src/main/java/org/example/Main.java
@@ -1,7 +1,35 @@
 package org.example;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        // Unchecked (runtime) -> exceptions que PODEM ser evitados se forem tratados e analisados pelo desenvolvedor
+        // Checked exception :  Exceptions que DEVEM ser eviytados e tratados  pelo desenvolvedor
+
+        String a  = JOptionPane.showInputDialog("numerador: ");
+        String b = JOptionPane.showInputDialog("denominador: ");
+
+
+try {
+    int resultado = dividir(Integer.parseInt(a), Integer.parseInt(b));
+
+    System.out.println("resultado  = " + resultado);
+
+} catch (NumberFormatException e) {
+    JOptionPane.showMessageDialog(null, "entrada invalida, informe um numero inteiro: "
+            + e.getMessage());
+//    e.printStackTrace();
+
+}finally {
+    System.out.println("Chegou no finally");
+}
+
+
     }
+
+
+    public static int dividir(int a, int b) {return a/b;}
 }
